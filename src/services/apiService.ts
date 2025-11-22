6d9fc407a5ccf1ceed07631b2945ef977bcf3c96
SarbBhangu <Bhangu513@gmail.com>
2025-11-22 11:11:35 -0500

    import product and errorHandler into apiService

---
+++ src/services/apiService.ts
@@ -0,0 +1,3 @@
+import {Product} from "../models/Product";
+import { ValidationError, handleError } from "../utils/errorHandler";
+
--- src/utils/discountCalculator.ts
+++ src/utils/discountCalculator.ts
@@ -1,3 +1,5 @@
 export function calculateDiscount(price: number, discountPercentage: number): number {
-        
+        const discountAmount = (price * discountPercentage) / 100
+        const finalPrice = price - discountAmount;
+        return finalPrice;
 }

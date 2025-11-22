3012de994344dfbfaae564b507e43e6307fd99b6
SarbBhangu <Bhangu513@gmail.com>
2025-11-22 12:10:47 -0500

    imported all TS files into main index.ts

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { Product } from "./models/Product";
+import { fetchProducts } from "./services/apiService";
+import { calculateDiscount } from "./utils/discountCalculator";
+import { calculateTax } from "./utils/taxCalculator";

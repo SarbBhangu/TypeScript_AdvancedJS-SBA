843b63497be0eff67f6eaf4657df88f42898a726
SarbBhangu <Bhangu513@gmail.com>
2025-11-22 11:57:04 -0500

    Add async fetchProducts function to apiService

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,6 +1,33 @@
 import {Product} from "../models/Product";
 import {ValidationError, handleError } from "../utils/errorHandler";
 
+
 const productURL = "https://dummyjson.com/products";
 
 
+export async function fetchProducts(): Promise<Product[]> {
+        try {
+        const response = await fetch(productURL);
+
+        if (!response.ok) {
+        throw new ValidationError("Failed to fetch products", response.status);
+        }
+
+        const data = await response.json();
+
+        const products: Product[] = data.products.map((item:any) => {
+        return new Product(
+            item.id,
+            item.title,
+            item.price,
+            item.discountPercentage,
+            item.category
+            );
+            });
+
+        return products;
+        } catch(error){
+            handleError(error);
+            throw error;
+        }
+    }
